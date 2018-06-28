b0338c8c207d02abf90382de341041de47247170
liumapp <liumapp.com@gmail.com>
2018-06-28 16:33:59 +0800

    add export certifcate and require

--- src/main/java/com/liumapp/jks/core/certificate/ExportCertificate.java
+++ src/main/java/com/liumapp/jks/core/certificate/ExportCertificate.java
@@ -1,21 +1,20 @@
 package com.liumapp.jks.core.certificate;
 
 import com.alibaba.fastjson.JSONObject;
-import com.liumapp.jks.core.certificate.require.GenerateCertificateRequire;
+import com.liumapp.jks.core.certificate.require.ExportCertificateRequire;
 import com.liumapp.jks.core.filter.RequestFilter;
 
 /**
  * @author liumapp
- * @file GenerateCertificate.java
+ * @file ExportCertificate.java
  * @email liumapp.com@gmail.com
  * @homepage http://www.liumapp.com
  * @date 6/28/18
  */
-public class GenerateCertificate extends RequestFilter<GenerateCertificateRequire> {
+public class ExportCertificate extends RequestFilter<ExportCertificateRequire> {
 
     @Override
-    public JSONObject handle(GenerateCertificateRequire data) {
-        
+    public JSONObject handle(ExportCertificateRequire data) {
         return null;
     }
 
---
+++ src/main/java/com/liumapp/jks/core/certificate/require/ExportCertificateRequire.java
@@ -0,0 +1,14 @@
+package com.liumapp.jks.core.certificate.require;
+
+import com.liumapp.jks.core.job.JobData;
+
+/**
+ * @author liumapp
+ * @file ExportCertificateRequire.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 6/28/18
+ */
+public class ExportCertificateRequire extends JobData {
+
+}
