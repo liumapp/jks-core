6da025cf0b47251e01d6a017dde64b947fe80d67
liumapp <liumapp.com@gmail.com>
2018-06-28 15:43:24 +0800

    rm unnecessary files and get generate jks done

--- src/main/java/com/liumapp/jks/core/container/GenerateJksContainer.java
+++ src/main/java/com/liumapp/jks/core/container/GenerateJksContainer.java
@@ -1,10 +1,18 @@
 package com.liumapp.jks.core.container;
 
 import com.alibaba.fastjson.JSONObject;
+import com.liumapp.jks.core.adapter.KeyTool;
 import com.liumapp.jks.core.container.require.GenerateJksContainerRequire;
 import com.liumapp.jks.core.filter.RequestFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.time.temporal.ChronoUnit;
 
 /**
  * @author liumapp
@@ -19,7 +27,25 @@ public class GenerateJksContainer extends RequestFilter<GenerateJksContainerRequ
 
     @Override
     public JSONObject handle(GenerateJksContainerRequire data) {
-
+        try {
+            String filename = data.getSavePath() + "/" + data.getKeyStoreName();
+            FileOutputStream out = new FileOutputStream(filename);
+            KeyTool.newKeyStore(data.getKeyStorePd())
+                    .newKeyPair()
+                    .keyLength(data.getKeyLength())
+                    .generateWithCertificate()
+                    .withValidity(1, ChronoUnit.YEARS)
+                    .withDistinguishName()
+                    .commonName(data.getFcName())
+                    .state(data.getFcCity())
+                    .locality(data.getFcProvince())
+                    .country(data.getFcCountry())
+                    .build()
+                    .createInKeyStore(data.getFcAlias(), data.getFcPassword())
+                    .writeTo(out);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+        }
         return null;
     }
 
