8b34ac2cf8137eae4e3b5e97d0de14ffd96aff76
liumapp <liumapp.com@gmail.com>
2018-06-28 09:46:33 +0800

    before detail job , need filter request

---
+++ src/main/java/com/liumapp/jks/core/container/GenerateJksContainer.java
@@ -0,0 +1,11 @@
+package com.liumapp.jks.core.container;
+
+/**
+ * @author liumapp
+ * @file GenerateJksContainer.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 6/28/18
+ */
+public class GenerateJksContainer {
+}
