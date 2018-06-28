18faa4c2936ef39f6547a178140618f767f290e7
liumapp <liumapp.com@gmail.com>
2018-06-28 11:05:48 +0800

    tmp

--- src/main/java/com/liumapp/jks/core/container/GenerateJksContainer.java
+++ src/main/java/com/liumapp/jks/core/container/GenerateJksContainer.java
@@ -22,27 +22,27 @@ public class GenerateJksContainer extends RequestFilter<GenerateJksContainerRequ
 
     @Override
     public JSONObject handle(GenerateJksContainerRequire data) {
-        try {
-            String fileName = data.getSavePath() + "/" + data.getKeyStoreName();
-            FileOutputStream out = new FileOutputStream(fileName);
-            KeyTools.newKeyStore(keyStorePattern.getKeyStorePd())
-                    .newKeyPair()
-                    .keyLength(keyStorePattern.getKeyLength())
-                    .generateWithCertificate()
-                    .withValidity(1 , ChronoUnit.YEARS)
-                    .withDistinguishName()
-                    .commonName(keyStorePattern.getFcName())
-                    .state(keyStorePattern.getFcCity())
-                    .locality(keyStorePattern.getFcProvince())
-                    .country(keyStorePattern.getFcCountry())
-                    .build()
-                    .createInKeyStore(keyStorePattern.getFcAlias() , keyStorePattern.getFcPassword())
-                    .writeTo(out);
-            out.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+//        try {
+//            String fileName = data.getSavePath() + "/" + data.getKeyStoreName();
+//            FileOutputStream out = new FileOutputStream(fileName);
+//            KeyTools.newKeyStore(keyStorePattern.getKeyStorePd())
+//                    .newKeyPair()
+//                    .keyLength(keyStorePattern.getKeyLength())
+//                    .generateWithCertificate()
+//                    .withValidity(1 , ChronoUnit.YEARS)
+//                    .withDistinguishName()
+//                    .commonName(keyStorePattern.getFcName())
+//                    .state(keyStorePattern.getFcCity())
+//                    .locality(keyStorePattern.getFcProvince())
+//                    .country(keyStorePattern.getFcCountry())
+//                    .build()
+//                    .createInKeyStore(keyStorePattern.getFcAlias() , keyStorePattern.getFcPassword())
+//                    .writeTo(out);
+//            out.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return null;
     }
 
 }
