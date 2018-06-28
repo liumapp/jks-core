d3762f8973769ee406457306e91b6745819f90f6
liumapp <liumapp.com@gmail.com>
2018-06-28 17:25:58 +0800

    add generate certificate require

--- src/main/java/com/liumapp/jks/core/certificate/require/GenerateCertificateRequire.java
+++ src/main/java/com/liumapp/jks/core/certificate/require/GenerateCertificateRequire.java
@@ -11,6 +11,97 @@ import com.liumapp.jks.core.job.JobData;
  */
 public class GenerateCertificateRequire extends JobData {
 
+    /**
+     * 密钥库地址
+     */
+    private String keystorePath;
+
+    /**
+     * 密钥库密码
+     */
+    private String storepass;
+
+    /**
+     * 别名
+     */
+    private String alias;
+
+    /**
+     * 个人证书密码
+     */
+    private String certPassword;
+
+    /**
+     * 加密算法
+     */
+    private String keyalg = "RSA";
+
+    /**
+     * 证书有效期天数
+     */
+    private Integer validity = 365;
+
+    /**
+     * 身份证号码
+     */
+    private String identityCode;
+
+    /**
+     * 密钥长度
+     */
+    private Integer keysize = 2048;
+
+    /**
+     * 私钥密码
+     */
+    private String keypass;
+
+    /**
+     * 完整的名字
+     */
+    private String name;
+
+    /**
+     * 密钥对拥有者名字
+     */
+    private String firstname;
+
+    /**
+     * 密钥对拥有者姓字
+     */
+    private String lastname;
+
+    /**
+     * 组织或者单位名称
+     */
+    private String organization;
+
+    /**
+     * 省份
+     */
+    private String province;
+
+    /**
+     * 城市
+     */
+    private String city;
+
+    /**
+     * 区域
+     */
+    private String area;
+
+    /**
+     * 国家代码
+     * 例如：中国，就填写CN
+     */
+    private String country;
+
+    /**
+     * 性别
+     * boy or girl or androgyne
+     */
+    private String sex;
 
 
 }
