ab519c1a994cb9f4826da66f40b81915b3bec6ad
liumapp <liumapp.com@gmail.com>
2018-06-29 09:31:56 +0800

    generate certificate done

--- src/main/java/com/liumapp/jks/core/certificate/GenerateCertificate.java
+++ src/main/java/com/liumapp/jks/core/certificate/GenerateCertificate.java
@@ -1,23 +1,47 @@
 package com.liumapp.jks.core.certificate;
 
 import com.alibaba.fastjson.JSONObject;
+import com.liumapp.jks.core.adapter.KeyStoreAdapter;
+import com.liumapp.jks.core.adapter.KeyTool;
 import com.liumapp.jks.core.certificate.require.GenerateCertificateRequire;
 import com.liumapp.jks.core.filter.RequestFilter;
 import com.liumapp.jks.core.loader.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.security.KeyStoreException;
+import java.time.temporal.ChronoUnit;
 
 /**
  * @author liumapp
  * @file GenerateCertificate.java
  * @email liumapp.com@gmail.com
  * @homepage http://www.liumapp.com
- * @date 6/28/18
+ * @date 6/29/18
  */
 public class GenerateCertificate extends RequestFilter<GenerateCertificateRequire> {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(GenerateCertificate.class);
+
     @Override
     public JSONObject handle(GenerateCertificateRequire data) {
-        Resource resource = Resource.from(data.getKeystorePath());
-
+        try {
+            Resource resource = Resource.from(data.getKeystorePath());
+            KeyStoreAdapter keyStoreAdapter = KeyTool.keyStoreFrom(resource , data.getStorepass());
+            keyStoreAdapter.newKeyPair()
+                    .keyLength(data.getKeysize())
+                    .generateWithCertificate()
+                    .withValidity(1, ChronoUnit.YEARS)
+                    .withDistinguishName()
+                    .commonName(data.getName())
+                    .state(data.getCity())
+                    .locality(data.getProvince())
+                    .country(data.getCountry())
+                    .build()
+                    .createInKeyStore(data.getAlias(), data.getCertPassword());
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+        }
         return null;
     }
 
