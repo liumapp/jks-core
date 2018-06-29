40b2212bdf4468e723d28d33be23e6b599185af8
liumapp <liumapp.com@gmail.com>
2018-06-29 09:33:36 +0800

    ready to write export certificate

--- src/main/java/com/liumapp/jks/core/certificate/ExportCertificate.java
+++ src/main/java/com/liumapp/jks/core/certificate/ExportCertificate.java
@@ -3,6 +3,8 @@ package com.liumapp.jks.core.certificate;
 import com.alibaba.fastjson.JSONObject;
 import com.liumapp.jks.core.certificate.require.ExportCertificateRequire;
 import com.liumapp.jks.core.filter.RequestFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author liumapp
@@ -12,6 +14,8 @@ import com.liumapp.jks.core.filter.RequestFilter;
  * @date 6/28/18
  */
 public class ExportCertificate extends RequestFilter<ExportCertificateRequire> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExportCertificate.class);
 
     @Override
     public JSONObject handle(ExportCertificateRequire data) {
