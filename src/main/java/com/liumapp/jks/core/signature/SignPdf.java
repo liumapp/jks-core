89b2ac157a979e9052371f69e0bea708017defb2
liumapp <liumapp.com@gmail.com>
2018-07-11 09:57:45 +0800

    add load key chain service

--- src/main/java/com/liumapp/jks/core/signature/SignPdf.java
+++ src/main/java/com/liumapp/jks/core/signature/SignPdf.java
@@ -2,6 +2,7 @@ package com.liumapp.jks.core.signature;
 
 import com.alibaba.fastjson.JSONObject;
 import com.liumapp.jks.core.filter.RequestFilter;
+import com.liumapp.jks.core.keypair.LoadKeyChainService;
 import com.liumapp.jks.core.signature.require.SignPdfRequire;
 
 /**
@@ -11,12 +12,12 @@ import com.liumapp.jks.core.signature.require.SignPdfRequire;
  * @homepage http://www.liumapp.com
  * @date 7/10/18
  */
-public class SignPdf extends RequestFilter<SignPdfRequire> {
+public class SignPdf extends RequestFilter<SignPdfRequire> implements LoadKeyChainService {
 
     @Override
     public JSONObject handle(SignPdfRequire data) {
         this.loggerRequest(data);
-        
+
         return null;
     }
 
