6b7633b29b8b24cd813097ac491273d5b11a3f0a
liumapp <liumapp.com@gmail.com>
2018-07-11 15:02:55 +0800

    add jks loader

---
+++ src/main/java/com/liumapp/jks/core/loader/JksLoader.java
@@ -0,0 +1,11 @@
+package com.liumapp.jks.core.loader;
+
+/**
+ * @author liumapp
+ * @file JksLoader.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/11/18
+ */
+public class JksLoader {
+}
