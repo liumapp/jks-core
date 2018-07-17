d4ec2a8af47718f78c2dd600456d6fcebf14cf14
liumapp <liumapp.com@gmail.com>
2018-07-17 11:48:29 +0800

    need two tmp file

--- src/main/java/com/liumapp/jks/core/signature/SignPdf.java
+++ src/main/java/com/liumapp/jks/core/signature/SignPdf.java
@@ -2,11 +2,14 @@ package com.liumapp.jks.core.signature;
 
 import com.alibaba.fastjson.JSONObject;
 import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
 import com.liumapp.jks.core.filter.RequestFilter;
 import com.liumapp.jks.core.signature.require.SignPdfRequire;
 import com.liumapp.jks.core.util.FileManager;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
 
@@ -22,15 +25,37 @@ public class SignPdf extends RequestFilter<SignPdfRequire> {
     @Override
     public JSONObject handle(SignPdfRequire data) {
         this.loggerRequest(data);
+        String tmpFile = this.getTmpFile(data.getPdfSavePath(), data.getPdfFileName());
         try {
             data.initSecurityInfo();
-            PdfReader pdfReader = new PdfReader(data.getPdfSavePath() + "/" + data.getPdfFileName());
-            String tmpFile = this.getTmpFile(data.getPdfSavePath(), data.getPdfFileName());
-            
+            PdfReader pdfReader = new PdfReader(tmpFile);
+            PdfStamper stamper = this.buildingStamper();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            File tmp = new File(tmpFile);
+            if (tmp.exists()) {
+                tmp.delete();
+            }
         }
         return null;
+    }
+
+    private PdfStamper buildingStamper (PdfReader pdfReader, String handlFile) {
+        FileOutputStream os = null;
+        PdfStamper stamper = null;
+        try {
+            os = new FileOutputStream(new File(handlFile));
+            stamper = PdfStamper.createSignature()
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                os.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private String getTmpFile (String savePath, String originalFileName) {
@@ -41,7 +66,7 @@ public class SignPdf extends RequestFilter<SignPdfRequire> {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return tmpName;
+        return savePath + "/" + tmpName;
     }
 
 }
