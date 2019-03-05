7f35c62a9a7dca12708ee5376efbd630adf3f0a6
liumapp <liumapp.com@gmail.com>
2019-03-05 11:49:03 +0800

    add qtools-file dependency to handle base64

--- src/main/java/com/liumapp/jks/core/signature/Base64StringSigner.java
+++ src/main/java/com/liumapp/jks/core/signature/Base64StringSigner.java
@@ -1,13 +1,38 @@
 package com.liumapp.jks.core.signature;
 
 import com.alibaba.fastjson.JSONObject;
+import com.itextpdf.awt.AsianFontMapper;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.*;
 import com.liumapp.jks.core.filter.RequestFilter;
 import com.liumapp.jks.core.signature.require.Base64StringSignerRequire;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Base64StringSigner extends RequestFilter<Base64StringSignerRequire> {
 
     @Override
     public JSONObject handle(Base64StringSignerRequire data) {
+        try {
+            PdfReader pdfReader = new PdfReader(data.getPdfPath());
+            PdfStamper stamper = new PdfStamper(pdfReader, new FileOutputStream(data.getResultPath()));
+            PdfContentByte canvas = stamper.getOverContent(data.getPage());
+            BaseFont baseFont = BaseFont.createFont(AsianFontMapper.ChineseSimplifiedFont,  AsianFontMapper.ChineseSimplifiedEncoding_H, BaseFont.NOT_EMBEDDED);
+            Font font = new Font(baseFont, 8);
+            ColumnText.showTextAligned(canvas, Element.ALIGN_LEFT, new Phrase(data.getContent(), font),
+                    data.getFirstX(), data.getFirstY(), 0);
+            stamper.close();
+            this.jobResult.put("result", "success");
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
--- src/main/java/com/liumapp/jks/core/signature/require/Base64StringSignerRequire.java
+++ src/main/java/com/liumapp/jks/core/signature/require/Base64StringSignerRequire.java
@@ -11,4 +11,82 @@ import com.liumapp.jks.core.job.JobData;
  * date 2019/3/5
  */
 public class Base64StringSignerRequire extends JobData {
+
+    private String pdfBase64;
+
+    private String content;
+
+    private float firstX;
+
+    private float firstY;
+
+    private int page;
+
+    private int fontSize;
+
+    public Base64StringSignerRequire() {
+    }
+
+    public Base64StringSignerRequire(String pdfBase64, String content, float firstX, float firstY, int page, int fontSize) {
+        this.pdfBase64 = pdfBase64;
+        this.content = content;
+        this.firstX = firstX;
+        this.firstY = firstY;
+        this.page = page;
+        this.fontSize = fontSize;
+    }
+
+    public String getPdfBase64() {
+        return pdfBase64;
+    }
+
+    public Base64StringSignerRequire setPdfBase64(String pdfBase64) {
+        this.pdfBase64 = pdfBase64;
+        return this;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Base64StringSignerRequire setContent(String content) {
+        this.content = content;
+        return this;
+    }
+
+    public float getFirstX() {
+        return firstX;
+    }
+
+    public Base64StringSignerRequire setFirstX(float firstX) {
+        this.firstX = firstX;
+        return this;
+    }
+
+    public float getFirstY() {
+        return firstY;
+    }
+
+    public Base64StringSignerRequire setFirstY(float firstY) {
+        this.firstY = firstY;
+        return this;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public Base64StringSignerRequire setPage(int page) {
+        this.page = page;
+        return this;
+    }
+
+    public int getFontSize() {
+        return fontSize;
+    }
+
+    public Base64StringSignerRequire setFontSize(int fontSize) {
+        this.fontSize = fontSize;
+        return this;
+    }
 }
